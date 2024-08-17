b5a3d373d0daa707f2745781eefd142b6a3b3608
prasad <prasad@gmail.com>
2024-08-17 23:21:26 +0530

    k8s

---
+++ variables.tf.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-04a8063bf88d0ec0c"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-0dd9e2f1d7aebc05c"
+}
